918d30b934cf763e2fad98b5e44c3dec7ab553be
William Luck <luck.william.h@gmail.com>
2022-08-05 19:20:04 -0500

    commit tree d199fb34ce5e7ffbb21e8adf7a0a61a4c0ea8014

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,38 @@
-# Your code goes here!
+require 'pry'
+
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagram_array)
+        match = []
+        anagram_array.map do |word|
+            characters = (word.chars).sort 
+            if (characters == @word.chars.sort) 
+                match.push(word)
+            end
+        end
+        match
+    end
+
+
+end
+
+binding.pry
+0
+
+
+
+# The method receives an array of words 
+# I want to enumerate over each element (word) of that array. For each element: 
+    # I want to get an array of characters
+    # I want to sort those characters in order
+    # Then comparte the end result of that sort to the initliazed word
+    
+    # If true, return that element of the array into a new array..
+
+
